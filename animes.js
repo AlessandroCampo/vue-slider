47fad9f3d3fe90d05d9e70972cb4d15bff5011fa
AlessandroCampo <alessandrocampo97@gmail.com>
2024-01-26 16:55:55 +0100

    importing from old project

---
+++ animes.js
@@ -0,0 +1,58 @@
+class Anime {
+    constructor({ title, plot, source }) {
+        this.title = title
+        this.plot = plot
+        this.source = source
+    }
+}
+
+const deathNote = new Anime({
+    title: "Death Note",
+    plot: "La storia racconta di Light, uno studente brillante che trova un Death Note e, grazie a esso, può uccidere chiunque semplicemente scrivendo il nome nel quaderno.",
+    source: "/img/deathnote.png"
+})
+
+const aot = new Anime({
+    title: "Attack on Titan (Shingeki no Kyojin)",
+    plot: "Ambientato in un mondo minacciato dai Titani, segue Eren, Mikasa e Armin che combattono per scoprire i segreti dietro di loro e difendere l'umanità contro le creature giganti.",
+    source: "/img/aot.jpg"
+})
+
+const fullMetalAlchemist = new Anime({
+    title: "Fullmetal Alchemist: Brotherhood",
+    plot: "Segue i fratelli Elric in un mondo di alchimia, alla ricerca della Pietra Filosofale e coinvolti in una cospirazione più ampia che coinvolge il governo e la verità oscura dietro la Pietra Filosofale.",
+    source: "/img/fullmetalalchemist.jpg"
+})
+
+const sevenDeadlySins = new Anime({
+    title: "Seven Deadly Sins (Nanatsu no Taizai)",
+    plot: "La principessa Elizabeth cerca l'aiuto dei Sette Peccati Capitali contro i Cavalieri Sacri corrotti. Mentre combattono contro forze oscure, emergono segreti e affrontano nemici potenti in una storia ricca di personaggi affascinanti e colpi di scena emozionanti.",
+    source: "/img/7ds.jpg"
+})
+
+const overlord = new Anime({
+    title: "Overlord",
+    plot: "Momonga, intrappolato in un mondo di gioco dopo la chiusura del server, esplora, domina con il suo avatar e affronta temi fantasy in un'avventura che esplora il concetto di essere intrappalato in un mondo virtuale.",
+    source: "/img/overlord.png"
+})
+
+const blue_lock = new Anime({
+    title: "Blue Lock",
+    plot: "Yoichi Isagi, promettente giocatore di calcio, entra in Blue Lock per diventare attaccante nazionale. All'interno, si svolgono intense rivalità e scoperte personali mentre affronta la competizione e il prezzo del successo nel mondo del calcio.",
+    source: "/img/bluelock.jpg"
+})
+
+const dbz = new Anime({
+    title: "Dragon Ball Z",
+    plot: "Goku, un potente guerriero, difende la Terra da minacce cosmiche, affrontando avversari sempre più forti. La storia esplora combattimenti epici, amicizia e la crescita di Goku come guerriero.",
+    source: "/img/dbz.jpg"
+})
+
+const hxh = new Anime({
+    title: "Hunter x Hunter",
+    plot: "Gon Freecss, giovane avventuriero, si imbarca in una missione per diventare un Cacciatore e trovare suo padre. Lungo il percorso, affronta sfide, stringe amicizie e scopre il mondo dei Cacciatori.",
+    source: "/img/hunterxhunter.jpg"
+})
+
+
+const allAnimes = [deathNote, aot, fullMetalAlchemist, sevenDeadlySins, overlord, blue_lock, dbz, hxh]
